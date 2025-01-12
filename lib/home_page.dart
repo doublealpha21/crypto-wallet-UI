e190fba5857b3790c630e0922086d582ec8a9245
doublealpha21 <praise8fakorede@gmail.com>
2025-01-12 23:15:42 +0100

    Home Page Edit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_font_icons/crypto_font_icons.dart';
 import 'package:crypto_ui/coin_tile.dart';
 import 'package:flutter/material.dart';
 
@@ -8,43 +9,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            height: 230,
-            decoration: const BoxDecoration(
-              color: Colors.teal,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.elliptical(200, 40),
-                bottomRight: Radius.elliptical(
-                  200,
-                  40,
-                ),
-              ),
-            ),
-            child: Align(
-              alignment: Alignment.center,
-              child: Text(
-                'TestCoin',
-                style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                      color: Colors.white,
-                    ),
+    return ListView(
+      children: [
+        Container(
+          height: 230,
+          decoration: const BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.elliptical(200, 60),
+              bottomRight: Radius.elliptical(
+                200,
+                60,
               ),
             ),
           ),
-          const SizedBox(
-            height: 20,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              'TestCoin',
+              style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
           ),
-          CoinTile(
-            requiredIcon: const Icon(Icons.currency_bitcoin),
-            title: 'BTC',
-            subtitle: 'Sample Value',
-            background: Colors.yellow[900],
-            onPressed: () {},
-          ),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        CoinTile(
+          requiredIcon: CryptoFontIcons.BTC,
+          title: 'BTC',
+          subtitle: 'Sample Value',
+          background: Colors.yellow[900],
+          onPressed: () {},
+        ),
+        CoinTile(
+          requiredIcon: CryptoFontIcons.ETH,
+          title: 'ETH',
+          subtitle: 'Sample Value',
+          background: Colors.purple,
+          onPressed: () {},
+        ),
+        CoinTile(
+          requiredIcon: CryptoFontIcons.DOGE,
+          title: 'DOGE',
+          subtitle: 'Sample Value',
+          background: Colors.yellowAccent,
+          onPressed: () {},
+        ),
+        CoinTile(
+          requiredIcon: CryptoFontIcons.XRP,
+          title: 'XRP',
+          subtitle: 'Sample Value',
+          background: Colors.black,
+          onPressed: () {},
+        ),
+        CoinTile(
+          requiredIcon: CryptoFontIcons.USDT,
+          title: 'USDT',
+          subtitle: 'Sample Value',
+          background: Colors.green,
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
